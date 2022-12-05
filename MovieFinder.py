84409ee555e71054db94f4205e5055d229c49fd1
Anithaus <anithus@gmail.com>
2022-12-04 16:03:12 -0800

    resolved conflict

--- MovieFinder.py
+++ MovieFinder.py
@@ -29,13 +29,6 @@ def themoviedbAPI(movie):
   apiLink = 'https://api.themoviedb.org/3/search/movie?api_key='+my_key+'&query='+movie
 
 
-#api call return the posterLinks
-def callApi():
-  # This is a comment
-  my_key = 'bcc4d5b1'
-  apiLink = ' http://www.omdbapi.com/?s=' + getTitle() + '&apikey=bcc4d5b1'
-
-
   try:
     r = requests.get(apiLink)
     data = r.json()
@@ -49,7 +42,6 @@ def callApi():
 movieNames = ['avenger', 'thing', 'black', 'wave', 'it', 'teen', 'john', 'the', 'him', 'one', 'hero']
 background =['secondary', 'success','warning','info','primary','danger']
 movieInformation = []
-
 
 def store_song(movie):
   movieI = themoviedbAPI(movie)
@@ -69,77 +61,6 @@ def index():
   random_number = random.randint(0, 6)
   random_color = random.randint(0, 5)
   return render_template('index.html', form=form, my_data=data,random_number=random_number,background=background,random_color=random_color)
-=======
-def posterLinks(data):
-  posterLinks = []
-  for movie in data['Search']:
-    print(movie['Title'], movie['Year'])
-    posterLinks.append(movie['Poster'])
-  return posterLinks
-
-
-
-# https://api.themoviedb.org/3/search/movie?api_key=933dcbef063eeaeab80f38d6e792311c&query=Life
-def themoviedbAPI(movie):
-  my_key = '933dcbef063eeaeab80f38d6e792311c'
-  apiLink = 'https://api.themoviedb.org/3/search/movie?api_key='+my_key+'&query='+movie
-
-
-  try:
-    r = requests.get(apiLink)
-    data = r.json()
-    # pprint(data)
-
-  except:
-    print('please try again')
-  return data
-
-movieNames = ['inception', 'thing', 'afterlife', 'wave']
-
-
-# route decorator binds a function to a URL
-# @app.route('/')
-# def hello():
-#   data=themoviedbAPI()
-#   random_number = random.randint(0, 10)
-#   pprint(data)
-#   return render_template('index.html',my_data=data,random_number=random_number)
-
-
-class Playlist(FlaskForm):
-  song_title = StringField(
-    'Movie Title', 
-    validators=[DataRequired()]
-  )
-
-
-movieNames = ['avenger', 'thing', 'black', 'wave', 'it', 'teen', 'john', 'the', 'him', 'one', 'hero']
-movieInformation = []
-def store_song(movie):
-  movieI = themoviedbAPI(movie)
-  movieInformation.append(movieI)
-
-
-@app.route('/', methods=('GET', 'POST'))
-def index():
-  form = Playlist()
-  if form.validate_on_submit():
-    movieInformation = []
-    store_song(form.song_title.data)
-    return redirect('/movie')
-
-  data=themoviedbAPI(random.choice(movieNames))
-  random_number = random.randint(0, 10)
-  # pprint(data)
-  return render_template('index.html', form=form, my_data=data,random_number=random_number)
-
-
-@app.route('/movie')
-def movie():
-  return render_template('index2.html', movieInfo=movieInformation)
-
-
-
 
 #movie page
 @app.route('/movie')
