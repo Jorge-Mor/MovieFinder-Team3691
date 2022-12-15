a4bb7b3751e86f23d996f09760983975c803473b
Anitha Rajamohan <85080610+Anithaus@users.noreply.github.com>
2022-12-14 17:06:33 -0800

    Anitha (#9)
    * added color to index2 page
    
    * added more info code
    
    Co-authored-by: Anithaus <anithus@gmail.com>

--- MovieFinder.py
+++ MovieFinder.py
@@ -1,3 +1,19 @@
+"""
+Course: CST 205
+Title: Movie Finder
+Abstract: In terms of the future of this project we want to be able to display more information about the movies we are displaying.
+We also wanted to get a second part of the website where the user can "save" these images and later manipulate them as they please.
+Our last idea was to show not just once movie when a movie is searched but everyone movie that shares a similar name in case the user does not exactly know the name of the movie they are looking for
+Authors:Anitha Rajamohan, Jorge Mereno, Kyle Borch
+Date: 12/14/2022
+Sources cited: https://www.themoviedb.org/documentation/api
+Work Distribution: Anitha Rajamohan and Jorge Mereno find the Api for the project
+Anitha Rajamohan: # index Api call and design
+                  # movieTitle(title) funtion and moreInfo page Styling and displaying data
+Jorge Mereno: # activated the serch button and rendered data to index2 page
+              # funtion worked on Playlist(FlaskForm), themoviedbAPI(movie), store_song(movie), index(),movie()
+Kyle Borch: worked on index2 Styling and displaying data in /movie page
+"""
 import requests, json
 from pprint import pprint
 from flask import Flask,render_template, flash, redirect
@@ -6,7 +22,6 @@ import random
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from datetime import datetime
 
 
 # create an instance of Flask
@@ -17,6 +32,7 @@ app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap5(app)
 
 #class for playlist
+# worked by Jorge Mereno
 class Playlist(FlaskForm):
   song_title = StringField(
     'Movie Title', 
@@ -24,6 +40,7 @@ class Playlist(FlaskForm):
   )
 
 #geting Api call
+# worked by Jorge Mereno
 def themoviedbAPI(movie):
   my_key = '933dcbef063eeaeab80f38d6e792311c'
   apiLink = 'https://api.themoviedb.org/3/search/movie?api_key='+my_key+'&query='+movie
@@ -32,22 +49,24 @@ def themoviedbAPI(movie):
   try:
     r = requests.get(apiLink)
     data = r.json()
-    # pprint(data)
 
   except:
     print('please try again')
   return data
 
 #Assigning movie names to background to get random colors and image on the home screen
+# worked by Jorge Mereno
 movieNames = ['avenger', 'thing', 'black', 'wave', 'it', 'teen', 'john', 'the', 'him', 'one', 'hero']
 background =['secondary', 'success','warning','info','primary','danger']
 movieInformation = []
 
+# worked by Jorge Mereno
 def store_song(movie):
   movieI = themoviedbAPI(movie)
   movieInformation.append(movieI)
 
 #home page
+# worked by Jorge Mereno, color added by Anitha Rajamohan
 @app.route('/', methods=('GET', 'POST'))
 def index():
   #Collecting info from the home page and rendering to a new page if the search button is clicked
@@ -63,6 +82,18 @@ def index():
   return render_template('index.html', form=form, my_data=data,random_number=random_number,background=background,random_color=random_color)
 
 #movie page
+# by Jorge Mereno
 @app.route('/movie')
 def movie():
   return render_template('index2.html', movieInfo=movieInformation)
+
+# movie page by title gived more information about movie
+# added by Anitha Rajamohan
+@app.route('/movie/<title>')
+def movieTitle(title):
+  random_color = random.randint(0, 5)
+  data=themoviedbAPI(title)
+  return render_template('moreInfo.html', movieInfo=data,background=background,random_color=random_color)
+
+
+
